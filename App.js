cc083e4022235837d1123145a23a5a8b57c8958b
carlos47fnaf <kaduardomello@gmail.com>
2023-03-29 13:33:56 -0300

    ATUALIZAR

--- App.js
+++ App.js
@@ -1,46 +1,63 @@
-import { StyleSheet, View, Text, ScrollView } from 'react-native';
-import Card from './src/components/Card';
-
+import { StyleSheet, View, Text, ScrollView } from "react-native";
+import Card from "./src/components/Card";
+import React from "react";
 
 export default function App() {
-
   const filmes = [
     {
-    id: 1,
-    imgfilme:"https://www.shoppingcidadedasflores.com.br/wp-content/uploads/sites/244/2023/03/12654_medio.jpg",
-    genero:'Ação',
-  },
-  {
-    id: 2,
-    imgfilme:"https://www.shoppingcidadedasflores.com.br/wp-content/uploads/sites/244/2023/03/12654_medio.jpg",
-    genero:'Ação',
-  },
-  {
-    id: 3,
-    imgfilme:"https://www.shoppingcidadedasflores.com.br/wp-content/uploads/sites/244/2023/03/12654_medio.jpg",
-    genero:'Ação',
-  },
-  {
-    id: 4,
-    imgfilme:"https://www.shoppingcidadedasflores.com.br/wp-content/uploads/sites/244/2023/03/12654_medio.jpg",
-    genero:'Ação',
-  },
-  {
-    id: 5,
-    imgfilme:"https://www.shoppingcidadedasflores.com.br/wp-content/uploads/sites/244/2023/03/12654_medio.jpg",
-    genero:'Ação',
-  },
-  ]
+      id: 1,
+      imgfilme:
+        "https://www.shoppingcidadedasflores.com.br/wp-content/uploads/sites/244/2023/03/12654_medio.jpg",
+      titulo: "Jhon Wick 4: Baba Yaga",
+      genero: "Ação",
+    },
+    {
+      id: 2,
+      imgfilme:
+        "https://a-static.mlcdn.com.br/1500x1500/poster-gigante-top-gun-maverick-editora-europa/eurobest/107032/e32e0a7f4b4d8888e8abbf4e281e8ffd.jpg",
+      titulo: "Top gun: Maverick",
+      genero: "Ação",
+    },
+    {
+      id: 3,
+      imgfilme:
+        "https://m.media-amazon.com/images/W/IMAGERENDERING_521856-T1/images/I/81NcnbKcqOL._AC_SX679_.jpg",
+      titulo: "Guardiões da galaxya: Volume 3",
+      genero: "Ação",
+    },
+    {
+      id: 4,
+      imgfilme:
+        "https://sm.ign.com/ign_br/screenshot/default/fn0dezjxoawjcym_92km.jpg",
+      titulo: "Velozes e furiosos X",
+      genero: "Ação",
+    },
+    {
+      id: 5,
+      imgfilme:
+        "https://uauposters.com.br/media/catalog/product/cache/1/thumbnail/800x930/9df78eab33525d08d6e5fb8d27136e95/8/5/855020221026-uau-posters-avatar-2-way-of-the-water-filmes.jpg",
+      titulo: "Avatar: O caminho da água",
+      genero: "Aventura",
+    },
+   
+  ];
 
-  return(
+  const removerItem = (id) => {
+    const index = filmes.findIndex((filme) => filme.id === id);
+    const novaLista = [...filmes];
+    novaLista.splice(index, 1);
+    setFilmes(novaLista);
+  };
+
+  return (
     <ScrollView>
       <View style={styles.container}>
-        <Text style={{ fontSize: 20}} >
-          Bem vindo.
-        </Text>
-      <View style={styles.main}>
-      {filmes.map(filmes => <Card filme={filme} key={filmes.id} />)}  
-      </View>  
+        <Text style={{ fontSize: 25, color: "white" }}>5 filmes online</Text>
+        <View style={styles.main}>
+          {filmes.map((filmes) => (
+            <Card filme={filmes} key={filmes.id} />
+          ))}
+        </View>
       </View>
     </ScrollView>
   );
@@ -48,19 +65,16 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#0d0',
-    alignItems: 'center',
-    justifyContent: 'center',
-    paddingTop: 20,
+    backgroundColor: "#0F0F0F",
+    alignItems: "center",
+    justifyContent: "center",
+    paddingTop: 15,
   },
   main: {
     flex: 1,
-    backgroundColor: '#ccc',
-    flexDirection: 'row',
-    flexWrap: 'wrap',
-    width: '100%',
-  }
-
+    backgroundColor: "#ccc",
+    flexDirection: "row",
+    flexWrap: "wrap",
+    width: "100%",
+  },
 });
-
- 
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,24 +1,30 @@
-import { View, Text, StyleSheet, Image } from 'react-native';
+import { View, Text, StyleSheet, Image, TouchableOpacity } from "react-native";
 
 export default function Card(props) {
   return (
     <View style={styles.card}>
-        <Image style={styles.imagem} source={{ uri: props.filme.imgfilme}} />  
-        <Text style={{ color: 'darkred'}}> {props.filme.genero} </Text>
+      <Image style={styles.imagem} source={{ uri: props.filme.imgfilme }} />
+      <Text style={{color: "white", margin: 2 }}>{props.filme.titulo}</Text>
+      <Text style={{ color: "white", fontWeight: "bold" }}> {props.filme.genero} </Text>
+      <TouchableOpacity onPress={props.removerItem} style={styles.botao}>
+        <Text style={{ color: "white", fontWeight: "bold" }}>Remover</Text>
+      </TouchableOpacity>
     </View>
   );
 }
 const styles = StyleSheet.create({
   card: {
-    backgroundColor: 'orange',
-    width: '40%',
-    height: 215,
+    backgroundColor: "#0F0F0F",
+    color: "white",
+    width: "40%",
+    height: 320,
     borderRadius: 10,
     margin: 15,
   },
   imagem: {
-    resizeMode: 'stretch',
-    width: '100%',
-    height: 180,
-  }
-});     
+    resizeMode: "stretch",
+    width: "100%",
+    height: 260,
+    borderRadius: 10,
+  },
+});
