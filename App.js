e1e8c34ec03909d83d851666fda4675b1fec8c4f
carlos47fnaf <kaduardomello@gmail.com>
2023-05-04 14:30:49 -0300

    novo

--- App.js
+++ App.js
@@ -1,63 +1,63 @@
-import { StyleSheet, View, Text, ScrollView } from "react-native";
+import { StyleSheet, View, Text, ScrollView, TextInput,  } from "react-native";
 import Card from "./src/components/Card";
 import React from "react";
+import { Image } from 'react-native';
+
 
 export default function App() {
   const [filmes, setFilmes] = React.useState([
     {
       id: 1,
       imgfilme:
-        "https://www.shoppingcidadedasflores.com.br/wp-content/uploads/sites/244/2023/03/12654_medio.jpg",
-      titulo: "Jhon Wick 4: Baba Yaga",
-      genero: "Ação",
+        "https://tfcprw.vtexassets.com/arquivos/ids/157344-1200-auto?v=637976645140470000&width=1200&height=auto&aspect=true",
+      titulo: "CAMISETA PLUS SIZE DUPLA FACE ONE PIECE LOGOS",
+      genero: "R$89,90",
     },
+    
     {
       id: 2,
       imgfilme:
-        "https://a-static.mlcdn.com.br/1500x1500/poster-gigante-top-gun-maverick-editora-europa/eurobest/107032/e32e0a7f4b4d8888e8abbf4e281e8ffd.jpg",
-      titulo: "Top gun: Maverick",
-      genero: "Ação",
+        "https://tfcprw.vtexassets.com/arquivos/ids/157344-1200-auto?v=637976645140470000&width=1200&height=auto&aspect=true",
+      titulo: "CAMISETA PLUS SIZE DUPLA FACE ONE PIECE LOGOS",
+      genero: "R$89,90",
     },
     {
       id: 3,
       imgfilme:
-        "https://m.media-amazon.com/images/W/IMAGERENDERING_521856-T1/images/I/81NcnbKcqOL._AC_SX679_.jpg",
-      titulo: "Guardiões da galaxya: Volume 3",
-      genero: "Ação",
+        "https://tfcprw.vtexassets.com/arquivos/ids/157344-1200-auto?v=637976645140470000&width=1200&height=auto&aspect=true",
+      titulo: "CAMISETA PLUS SIZE DUPLA FACE ONE PIECE LOGOS",
+      genero: "R$89,90",
     },
     {
       id: 4,
       imgfilme:
-        "https://sm.ign.com/ign_br/screenshot/default/fn0dezjxoawjcym_92km.jpg",
-      titulo: "Velozes e furiosos X",
-      genero: "Ação",
+        "https://tfcprw.vtexassets.com/arquivos/ids/157344-1200-auto?v=637976645140470000&width=1200&height=auto&aspect=true",
+      titulo: "CAMISETA PLUS SIZE DUPLA FACE ONE PIECE LOGOS",
+      genero: "R$89,90",
     },
     {
       id: 5,
       imgfilme:
-        "https://uauposters.com.br/media/catalog/product/cache/1/thumbnail/800x930/9df78eab33525d08d6e5fb8d27136e95/8/5/855020221026-uau-posters-avatar-2-way-of-the-water-filmes.jpg",
-      titulo: "Avatar: O caminho da água",
-      genero: "Aventura",
+        "https://tfcprw.vtexassets.com/arquivos/ids/157344-1200-auto?v=637976645140470000&width=1200&height=auto&aspect=true",
+      titulo: "CAMISETA PLUS SIZE DUPLA FACE ONE PIECE LOGOS",
+      genero: "R$89,90",
     },
-   
+    
   ]);
-
-  const removerItem = (id) => {
-    const index = filmes.findIndex((filme) => filme.id === id);
-    const novaLista = [...filmes];
-    novaLista.splice(index, 1);
-    setFilmes(novaLista);
-  };
 
   return (
     <ScrollView>
       <View style={styles.container}>
-        <Text style={{ fontSize: 25, color: "white" }}>5 filmes online</Text>
+        <View style={styles.header}>
+        <Text style={{ fontSize: 25, color: "orange", fontWeight: "bold"}}>TechnoGeek</Text>
+        <TextInput style={{ backgroundColor: "white", borderRadius: "5px"}} />
+        <Text style={{fontSize: "200%"}}>👨‍🦱</Text>
+        </View>
+        <View style={{fontSize: "120%", fontWeight: "bold", color: "white" }}>Mais vendidos da semana</View>
         <View style={styles.main}>
           {filmes.map((filmes) => (
             <Card 
             filme={filmes} 
-            removerItem={() => removerItem(filmes.id)} 
             key={filmes.id} />
           ))}
         </View>
@@ -68,16 +68,22 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#0F0F0F",
+    backgroundColor: "purple",
     alignItems: "center",
     justifyContent: "center",
     paddingTop: 15,
+    
   },
   main: {
     flex: 1,
-    backgroundColor: "#ccc",
+    backgroundColor: "#140028",
     flexDirection: "row",
     flexWrap: "wrap",
     width: "100%",
   },
+  header: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+  }
 });
--- src/components/Card.js
+++ src/components/Card.js
@@ -4,20 +4,19 @@ export default function Card(props) {
   return (
     <View style={styles.card}>
       <Image style={styles.imagem} source={{ uri: props.filme.imgfilme }} />
-      <Text style={{color: "white", margin: 2 }}>{props.filme.titulo}</Text>
-      <Text style={{ color: "white", fontWeight: "bold" }}> {props.filme.genero} </Text>
+      <Text style={{color: "black", margin: 2, fontWeight: "bold" }}>{props.filme.titulo}</Text>
+      <Text style={{colo: "grey", margin: 2, fontWeight: "semiBold"}}>{props.filme.genero}</Text>
       <TouchableOpacity onPress={props.removerItem} style={styles.botao}>
-        <Text style={{ color: "white", fontWeight: "bold" }}>Remover</Text>
+        <Text style={{ color: "orange", fontWeight: "bold" }}>Comprar</Text>
       </TouchableOpacity>
     </View>
   );
 }
 const styles = StyleSheet.create({
   card: {
-    backgroundColor: "#0F0F0F",
-    color: "white",
+    backgroundColor: "white",
     width: "40%",
-    height: 320,
+    height: 350,
     borderRadius: 10,
     margin: 15,
   },
